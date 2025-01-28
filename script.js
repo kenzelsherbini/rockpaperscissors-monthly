6001a631c37d6a7d6017393b474842595e3b838a
kenzelsherbini <526kelsherbini@frhsd.com>
2025-01-28 10:43:54 -0500

    called fucntions and displayed pictures

--- script.js
+++ script.js
@@ -1,68 +1,71 @@
 //get username 
 let userName = prompt("What is your name?");
 document.getElementById("userName").textContent = userName;
+let userChoiceImg = document.getElementById('userChoiceImg')
+let computerChoiceImg = document.getElementById('computerChoiceImg')
 
+let computerChoice
+let userChoice
 
-//get user choice
-let userChoice = '';
-const updateUserChoice = (choice) => {
-    let imgSrc = '';
-    if (choice === 'rock') {
-      imgSrc = '/imgs/rock.png';
-    } else if (choice === 'paper') {
-      imgSrc = '/imgs/paper.png';
-    } else if (choice === 'scissors') {
-      imgSrc = '/imgs/scissors.png';
-    }
-    userChoiceImg.src = imgSrc;
-  };
 
 //get computer choice
-const updateComputerChoice = (choice) => {
-  let imgSrc = '';
-  if (choice === 'rock') {
-    imgSrc = '/imgs/rock.png';
-  } else if (choice === 'paper') {
-    imgSrc = '/imgs/paper.png';
-  } else if (choice === 'scissors') {
-    imgSrc = '/imgs/scissors.png';
-  }
-  computerChoiceImg.src = imgSrc;
-};
-
-const getComputerChoice = () => {
-  const randomNumber = Math.floor(Math.random() * 3);
-  
+let  getComputerChoice = () => {
+  let randomNumber = Math.floor(Math.random() * 3);
+console.log(randomNumber)
   switch (randomNumber) {
     case 0:
-      return 'rock';
+      computerChoiceImg.src= '/imgs/rock.png';
+      computerChoice='rock';
+      break
     case 1:
-      return 'paper';
+    computerChoiceImg.src = '/imgs/paper.png';
+     computerChoice='paper';
+     break
     case 2:
-      return 'scissors';
+      computerChoiceImg.src = '/imgs/scissors.png';
+      computerChoice='scissors';
+      break
+  }
+};
+
+//get user choice
+
+const updateUserChoice = (choice) => {
+  
+  if (choice === 'rock') {
+    userChoiceImg.src  = '/imgs/rock.png';
+  } else if (choice === 'paper') {
+    userChoiceImg.src  = '/imgs/paper.png';
+  } else if (choice === 'scissors') {
+    userChoiceImg.src  = '/imgs/scissors.png';
   }
 };
 
 
 //see who wins -> check player vs game
 const determineWinner = (userChoice, computerChoice) => {
-    
-    if (userChoice === computerChoice) {
-      return 'The game is a tie!';
-    }
-    
-    if (userChoice === 'rock') {
-      return computerChoice === 'paper' ? 'The computer won!' : 'You won!';
-    }
-    
-    if (userChoice === 'paper') {
-      return computerChoice === 'scissors' ? 'The computer won!' : 'You won!';
-    }
-    
-    if (userChoice === 'scissors') {
-      return computerChoice === 'rock' ? 'The computer won!' : 'You won!';
-    }
-  };
+
+  if (userChoice === computerChoice) {
+    return 'The game is a tie!';
+  }
+
+  if (userChoice === 'rock') {
+    return computerChoice === 'paper' ? 'The computer won!' : 'You won!';
+  }
+
+  if (userChoice === 'paper') {
+    return computerChoice === 'scissors' ? 'The computer won!' : 'You won!';
+  }
+
+  if (userChoice === 'scissors') {
+    return computerChoice === 'rock' ? 'The computer won!' : 'You won!';
+  }
+};
 
 
-playGame();
+function playGame(userChoice){
+ updateUserChoice(userChoice)
+ getComputerChoice()
+ determineWinner(userChoice, computerChoice)
+
+}
